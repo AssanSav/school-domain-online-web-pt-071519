3030efb6685f66ada48ad8cff6e094e3fa72b9d9
assane savadogo <assane.savadogo81@gmail.com>
2019-08-28 04:44:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,35 +1,3 @@
 require 'pry'
-class School 
-  attr_accessor :roster, :name 
+
   
-  def initialize(name)
-    @name = name 
-    @roster = {}
-  end
-  
-  def add_student(name, grade)
-    @roster[grade] ||= []
-    @roster[grade] << name
-  end
-  
-  def grade(grade)
-    new_arr = []
-    @roster.select do |key, value| 
-      if key == grade 
-         new_arr << value
-      #binding.pry
-      end
-    end
-    new_arr.flatten
-  end
-  
-  def sort 
-    new_hash = {}
-    @roster.each do |grade, name|
-      new_hash[grade] = name.sort!
-    end
-    new_hash
-  end
-  
-  
-end
